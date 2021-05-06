c1e04ce29493878742f1da4c7dc103b9a0633c21
sehdev kumar <55868273+sehdevkumar@users.noreply.github.com>
2021-05-06 20:50:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,29 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    
+    
     constructor(props) {
-        super(props);
+        super(props);
     this.state = {
-      isShow: false
+      isShow:""
     }
     this.btnClick = this.btnClick.bind(this);
-    };
-    
+    };
+    
   btnClick() {
     //console.log('btn clicked');
-    this.setState({isShow: true})
+    this.setState({isShow: <p id="para"><p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p></p>});
   }
-    
+    
     render() {
-        return(
-            <div id="main">
-                { /* Do not remove this main div!! */ }
-      <h1>hello manisha</h1>
+        return(
+            <div id="main">
+                { /* Do not remove this main div!! */ }
+      <h1>Hello Sehdev Kumar</h1>
       <button id="click" onClick={this.btnClick}>click me</button>
-      {this.state.isShow && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
-            </div>
-        );
+      {this.state.isShow}
+            </div>
+        );
     }
 }
 
